2166e6cedb69ec669db6c3061c4a3fe37d5d43f2
LayMorja <evil-corporaty@yandex.ru>
2022-12-20 16:33:31 +0300

    Second commit

--- config/tasks/css.js
+++ config/tasks/css.js
@@ -1,12 +1,21 @@
 import dartSass from "sass";
 import gulpSass from "gulp-sass";
-import GulpCleanCss from "gulp-clean-css";
+import cleanCss from "gulp-clean-css";
+import autoPrefixer from "gulp-autoprefixer";
 const sass = gulpSass(dartSass);
 
 export function css() {
    return app.gulp.src(app.path.src.styles)
-      .pipe(sass.sync({}).on("error", sass.logError))
-      .pipe(GulpCleanCss({}))
+      .pipe(sass.sync({
+         indentWidth: 3,
+      }).on("error", sass.logError))
+      .pipe(autoPrefixer({
+         grid: true,
+         overrideBrowserslist: ["last 3 versions"],
+         cascade: false,
+      }))
+      .pipe(app.gulp.dest(app.path.build.styles))
+      .pipe(cleanCss({}))
       .pipe(app.plugins.rename({
          extname: ".min.css",
       }))
--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,27 @@ import { plugins } from "./config/settings/plugins.js";
 //* Подключение путей
 import { path } from "./config/settings/path.js";
 
+//* Глобальная переменная для доступа отовсюду
 global.app = {
    path: path,
    gulp: gulp,
    plugins: plugins,
 }
 
+//* Импорт задач
+import { reset } from "./config/tasks/reset.js";
 import { css } from "./config/tasks/css.js";
 
+//* Слежка за изменениями
+function watcher() {
+   gulp.watch(["./src/scss/**.*.scss"], css);
+}
+
+//* Сценарии выполнения
+const devTasks = gulp.series(reset, css, watcher);
+
+//* Экспорт задач
 export { css };
+
+//* Экспорт сценариев выполнения
+export { devTasks };
